ade84a17c3e48d5dbf52a7a05cee7ebad19baa44
kvvzr <kwzr@twinkrun.net>
2015-11-04 01:35:34 +0900

    非公開のコメントを非表示にした

--- app/scripts/sora.js
+++ app/scripts/sora.js
@@ -2,8 +2,17 @@
 
 // 愛のあるsetInterval
 setInterval(function() {
-    var btns = document.getElementsByName('adminflg');
-    for (var i = 0; i < btns.length; i++) {
-        btns[i].checked = true;
+    var chks = document.getElementsByName('adminflg');
+    for (var i = 0; i < chks.length; i++) {
+        chks[i].checked = true;
+    }
+    var comment_list = document.getElementsByClassName('comment_list');
+    for (var i = 0; i < comment_list.length; i++) {
+        var comments = comment_list[i].childNodes;
+        for (var j = 0; j < comments.length; j++) {
+            if (comments[j].textContent.indexOf('[このコメントはメッセージ管理者のみが確認できます]') >= 0) {
+                comment_list[i].removeChild(comments[j]);
+            }
+        }
     }
 }, 1000);
